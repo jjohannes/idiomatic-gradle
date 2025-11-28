dbd9b5f82c6313c4b39f0d2b9e3b3aff45d0959f
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-11-28 15:11:35 +0100

    Cleanup in base.versioning.gradle.kts

--- gradle-conventions/src/main/kotlin/com.example.idiomatic.gradle.build.base.versioning.gradle.kts
+++ gradle-conventions/src/main/kotlin/com.example.idiomatic.gradle.build.base.versioning.gradle.kts
@@ -1,3 +1,2 @@
-// The version of the produc itself is read from the 'version.txt' file in the root of a product.
-@Suppress("UnstableApiUsage")
+// The version of the product itself is read from the 'version.txt' file in the root of a product.
 version = File(rootDir, "version.txt").readText().trim()
